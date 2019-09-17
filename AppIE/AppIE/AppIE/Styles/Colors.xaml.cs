fb07efa3a0359681417795e9ea4945930b9d6573
Enrique David Jose Incio Chapilliquen <enrique.incio@gmail.com>
2019-09-17 03:33:04 -0500

    avance noche 16-09-2019

---
+++ AppIE/AppIE/AppIE/Styles/Colors.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppIE.Styles
+{
+    
+    public partial class Colors 
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
